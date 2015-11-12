b688b8265be1c2a83ff1201438702fc1abd174e9
Kunal Limaye <kunallimaye@gmail.com>
2015-11-12 15:09:57 +1100

    Using properties for URI. Default part of the property substitution works. BUT not able to read the property file yet.

--- src/main/java/me/finiteloop/stores/online/search/SearchService.java
+++ src/main/java/me/finiteloop/stores/online/search/SearchService.java
@@ -1,5 +1,7 @@
 package me.finiteloop.stores.online.search;
 
+import javax.ws.rs.core.Response;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -18,20 +20,21 @@ public class SearchService
 	@Override
 	public void configure() throws Exception {
 		
-		restConfiguration().component("restlet").host("localhost").port(8182).bindingMode(RestBindingMode.auto);
+		// Properties resolution allows URI to be substituted at runtime.
+		// Use of colon ':' allows specification of a default value.
+		restConfiguration().component("restlet").host("{{uri.hostname.consumer:localhost}}")
+			.port("{{uri.port.consumer:8182}}").bindingMode(RestBindingMode.auto);
 		
 		rest("/search")
 			.consumes("application/json")
 			.produces("application/json")
 		.get("/")
 			.to("direct:get-search-criteria");
-		
-		
+				
 		from("direct:get-search-criteria")
 			.log("Search criteria is: ${body}")
 			.setBody(constant(""))
-			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant("200"));
+			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(Response.Status.OK.getStatusCode() + ""));
 	}
 
-	
 }
